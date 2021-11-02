b9d31c8380665a57ea8c8c5496bf83f6b32930a2
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-11-02 19:28:25 -0200

    Update animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 
 $(function(){
 
-    $(".info-dot").on('click', function(){
+    $(".info-dot").on('click', function(e){
+        e.stopPropagation();
         $(this).find(".info-detail").addClass("show");
         $(".info-dot").not(this).find(".info-detail").removeClass("show");
 
+    });
+
+    $(document).on('click', function(){
+        $(".info-detail").removeClass("show");
+    });
+
+    $("info-detail").on('click', function(e){
+        e.stopPropagation();
     });
 });
